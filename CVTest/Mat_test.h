bf6b8b93b7b7aa6375cb1fc9ee48694bd2b10de4
haigen.zhu <haigen.zhu@tcl.com>
2020-04-09 18:35:17 +0800

    add init

---
+++ CVTest/Mat_test.h
@@ -0,0 +1,1 @@
+void Mat_test_main();
---
+++ CVTest/main.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include"Mat_test.h"
+#include"read_test.h"
+using namespace cv;
+
+int main()
+{
+    //Mat_test_main();
+    read_test_main();
+    return 0;
+}
